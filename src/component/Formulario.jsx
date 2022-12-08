d976a9007a79516db0078e52394b23bae1bcc2f3
Karine Ferreira de Oliveira <30732416+karineferreiradeoliveira@users.noreply.github.com>
2022-12-07 22:09:47 -0300

    update_formulario

--- src/component/Formulario.jsx
+++ src/component/Formulario.jsx
@@ -8,13 +8,15 @@ class formulario extends Nullstack {
   render({ dica }) {
     return (
       <>
-        <div> formulario </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quidem,
-          assumenda quae similique error illum necessitatibus minima suscipit
-          quisquam numquam saepe eos id dignissimos hic. Similique adipisci quia
-          inventore a? <b>{dica}</b>{" "}
-        </p>
+        <main class="content-main">
+          <div class="titulo-form"> formulario </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quidem,
+            assumenda quae similique error illum necessitatibus minima suscipit
+            quisquam numquam saepe eos id dignissimos hic. Similique adipisci
+            quia inventore a e agora? <b>{dica}</b>{" "}
+          </p>
+        </main>
       </>
     );
   }
